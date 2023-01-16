7dfe171990a22a98c3ea92550aa4afc83e45879e
Marina Panina <paninamar235@gmail.com>
2023-01-16 18:32:52 +1000

    Rebuild project

---
+++ src/main/java/web/services/UserService.java
@@ -0,0 +1,43 @@
+package web.services;
+
+import web.dao.UserDao;
+import web.entities.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    private UserDao userDao;
+    @Autowired
+    public UserService(UserDao userDao) {
+        this.userDao = userDao;
+    }
+    public void add(User user){
+        userDao.add(user);
+    }
+
+    public List<User> getAllUsers(){
+        return userDao.getAllUsers();
+    }
+
+    public User getUserById(int id) {
+        return userDao.getUserById(id);
+    }
+
+    public void removeUserById(int id){
+        userDao.removeUserById(id);
+    }
+
+    public void removeUser(User user){
+        userDao.removeUser(user);
+    }
+
+    public void removeAllUsers(){
+        userDao.removeAllUsers();
+    }
+    public void update(int id, User user) {
+        userDao.update(id, user);
+    }
+}
